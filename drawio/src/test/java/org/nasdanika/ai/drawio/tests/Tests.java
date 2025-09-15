530f2a85fa755e6b250cc5375ca6d2376f120eb4
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-09-15 18:54:33 -0400

    Commented out draw.io a section

--- drawio/src/test/java/org/nasdanika/ai/drawio/tests/Tests.java
+++ drawio/src/test/java/org/nasdanika/ai/drawio/tests/Tests.java
@@ -6,22 +6,22 @@ import org.junit.jupiter.api.Test;
 import org.nasdanika.common.PrintStreamProgressMonitor;
 import org.nasdanika.common.Section;
 import org.nasdanika.drawio.Document;
-import org.nasdanika.drawio.gen.section.DrawioSectionGenerator;
+//import org.nasdanika.drawio.gen.section.DrawioSectionGenerator;
 
 import reactor.core.publisher.Flux;
 
 public class Tests {
 	
-	@Test
-	public void testSectionGenerator() throws Exception {
-		Document document = Document.load(getClass().getResource("alice-bob.drawio"));
-		DrawioSectionGenerator sectionGenerator = new DrawioSectionGenerator();
-		Flux<Section> sectionFlux = sectionGenerator.creatSectionsAsync(document, new PrintStreamProgressMonitor());
-		List<Section> sections = sectionFlux.collectList().block();
-		System.out.println(sections.size());
-		sections.forEach(section -> {
-			System.out.println(section.getTitle() + ": " + section.getId() + " " + section.getContents() + " " + section.getChildren());
-		});
-	}
+//	@Test
+//	public void testSectionGenerator() throws Exception {
+//		Document document = Document.load(getClass().getResource("alice-bob.drawio"));
+//		DrawioSectionGenerator sectionGenerator = new DrawioSectionGenerator();
+//		Flux<Section> sectionFlux = sectionGenerator.creatSectionsAsync(document, new PrintStreamProgressMonitor());
+//		List<Section> sections = sectionFlux.collectList().block();
+//		System.out.println(sections.size());
+//		sections.forEach(section -> {
+//			System.out.println(section.getTitle() + ": " + section.getId() + " " + section.getContents() + " " + section.getChildren());
+//		});
+//	}
 		
 }
